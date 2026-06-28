1f322094e70a11228ebccc8b502e07e386affaf3
Cristian Cesar <c.cesargutierrez789@gmail.com>
2026-06-27 22:13:49 -0400

    Agrega validación para que el nombre no esté vacío

--- saludo.py
+++ saludo.py
@@ -1,3 +1,10 @@
 def saludar(nombre_completo):
-return f"Hola {nombre_completo}"
+
+    if nombre_completo.strip() == "":
+        return "Error: El nombre no puede estar vacío."
+
+    return f"Hola {nombre_completo}"
+
+
 print(saludar("GitHub"))
+print(saludar(""))
